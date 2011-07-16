5a5916957ff4d206b83a5e915c20bc38a7e727ac
Garrett Robinson <garrett.f.robinson@gmail.com>
2011-07-15 18:25:26 -0700

    Using FileBrowseField instead of my custom code. A much better solution!

--- gallery/models.py
+++ gallery/models.py
@@ -1,10 +1,20 @@
 from django.db import models
 from mezzanine.pages.models import Page
+from filebrowser_safe.fields import FileBrowseField
 
-# All members of Page will be inherited by Gallery
 class Gallery(Page):
     notes = models.TextField("Notes")
 
 class GalleryImage(models.Model):
     gallery = models.ForeignKey("Gallery")
-    image = models.ImageField(upload_to="galleries")
+    image = FileBrowseField("Image", max_length=200, 
+            extensions=['.jpg','.png','.gif'])
+    text = models.TextField(blank=True)
+
+    class Admin:
+        pass
+
+    def __unicode__(self):
+        return u'%s' % self.text
+
+
